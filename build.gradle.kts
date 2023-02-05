e3b959aad98ce584651e8a3204c2e1197dfc6a40
schnaps <ulyanov@amediateka.ru>
2023-02-06 02:08:09 +0700

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+buildscript {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+        jcenter()
+
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
+
+allprojects {
+    repositories {
+        //google()
+//        jcenter()
+//        mavenCentral()
+   }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
